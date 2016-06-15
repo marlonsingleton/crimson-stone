b6bd80611389ebf88c23d8a6eec8938968f67616
Marlon Singleton <marlonosingleton@yahoo.com>
2016-06-15 13:46:29 +0300

    ROTD13

---
+++ practice/ROT13.rb
@@ -0,0 +1,22 @@
+def rot13(string)
+  num_result = string.split.map do |word|
+    word.chars.map do |char|
+      if ("A".."M").cover?(char) || ("a".."m").cover?(char)
+        char.ord + 13
+      elsif ("N".."Z").cover?(char) || ("n".."z").cover?(char)
+        char.ord - 13
+      else
+        char
+      end
+    end
+  end
+  char_result = num_result.map do |num_array| 
+    num_array.map { |num| num.chr }.join
+  end
+  char_result.join(" ")
+end
+
+phrase = "Hello the ROTD13 cipher will be applied to this phrase."
+
+p phrase
+p rot13(phrase)
