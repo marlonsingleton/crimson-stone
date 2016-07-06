19252760696bc820d5c388336326e7a4dc9a33f3
Marlon Singleton <marlonosingleton@yahoo.com>
2016-07-06 20:11:51 +0300

    oo work

---
+++ practice/greetings.rb
@@ -0,0 +1,23 @@
+class Greeting
+  def greet(message)
+    puts message
+  end
+end
+
+class Hello < Greeting
+  def hi
+    greet("Hello")
+  end
+end
+
+class Goodbye < Greeting
+  def bye
+    greet("Goodbye")
+  end
+end
+
+say_hi = Hello.new
+say_hi.hi
+say_bye = Goodbye.new
+say_bye.bye
+
